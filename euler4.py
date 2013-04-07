f51990e3431717705a5a8903e8f079eaef498e11
moochea <moochea@gmail.com>
2013-04-07 17:29:50 +1000

    completed puzzle 4

--- euler4.py
+++ euler4.py
@@ -1,8 +1,5 @@
 #!/usr/bin/env python
 
-import sys
-
-
 # palindrom x*x = number where number is the same read back and forth
 # abccba
 
@@ -19,5 +16,29 @@ def check_palindrome(num):
 # Check palindromes which are 6 digit numbers, from 100001 to 999999?
 # Looking for largest, start from 999999
 
+x1=999
+x2=999
+newvalue=580085
 
-#Adding more crap   
+print newvalue
+
+while x1>100:
+    result = x1*x2
+#    print "%d * %d = %d " % (x1,x2,result)
+    if result > newvalue:
+        if check_palindrome(result):
+            newvalue = result
+            print newvalue
+            x2=999
+            x1-=1
+        else:
+            if x2>101:
+                x2-=1
+            else:
+                x2=999
+                x1-=1
+    else:
+        x2=999
+        x1-=1
+
+
