3ebf14a763544d9c3b53eb64d503ef9f8005e3c5
Pei-Chea Tan <pei-chea.tan@leicabiosystems.com>
2022-03-28 07:48:24 +1100

    intro_to_data_Science pandas notebooks

---
+++ intro_to_data_sci/main.py
@@ -0,0 +1,18 @@
+
+import numpy as np
+import math
+
+
+def run_this():
+    some_list = [1,2,3,4,5]
+    a = np.array(some_list)
+    print(a)
+    print(a.ndim)
+
+
+def main():
+    run_this()
+
+
+if __name__ == "__main__":
+    main()
